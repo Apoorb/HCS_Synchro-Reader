241eaf1226d1644003c4eb0ec6a9e54cb0881fac
Apoorba Bibeka <apoorb2510@gmail.com>
2019-07-25 16:37:32 -0400

    Better way of reading Synchro

---
+++ LPGA-Synchro-Read-Approach2.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 15:40:38 2019
+#muy bien
+@author: abibeka
+"""
+
+#0.0 Housekeeping. Clear variable space
+from IPython import get_ipython  #run magic commands
+ipython = get_ipython()
+ipython.magic("reset -f")
+ipython = get_ipython()
+
+
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import re
+from io import StringIO
+
+
+
+def GetRelevantDat(file,SkRow=0):
+    Dat = pd.read_csv(file,skiprows=SkRow+1,nrows=36,sep='\t')
+    Dat2 = pd.read_csv(file,skiprows=SkRow+47,nrows=2,sep='\t')
+    Dat2.index = Dat2.index.droplevel([1,2,3])
+    Dat.columns =[col.strip() for col in Dat.columns]
+     # Get only rows I need and transpose
+    Results = Dat[Dat.Movement.isin(['V/C Ratio(X)          ','LnGrp Delay(d),s/veh  ',
+                                       'LnGrp LOS             ','' ,'%ile BackOfQ(95%),veh/ln',
+                                       'Approach Delay, s/veh ','Approach LOS          '])].transpose()
+    #Correct the columns
+    Results.columns = Results.iloc[0]
+    Results =Results.drop(Results.index[0])
+    Results.columns = ["V/C_Ratio","Q_95Per","LnGrpDelay","LnGrpLOS","ApproachDelay","ApproachLOS"]
+    Results["QLen"] = Results.Q_95Per.apply(pd.to_numeric, errors='coerce').round(0)*25
+    #Subset data
+    Results1 =  Results[["V/C_Ratio","LnGrpDelay","LnGrpLOS","QLen","ApproachDelay","ApproachLOS"]]
+    
+    #Get the order of the rows same as the table in word
+    Results1.index =pd.Categorical(Results1.index,[ 'WBL','WBT', 'WBR',
+                                   'NBL', 'NBT','NBR', 
+                                   'EBL', 'EBT', 'EBR', 
+                                   'SBL', 'SBT', 'SBR'])
+        
+    Results1.LnGrpLOS =Results1[['LnGrpDelay','LnGrpLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
+    #Get the format of the Qlen and Delay same as the table in word
+    Results1.ApproachLOS =Results1[['ApproachDelay','ApproachLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
+    Results1.QLen = Results1.QLen.apply(lambda x: int(x) if x == x else "")
+    Results1.QLen = Results1.QLen.apply(lambda x: format(int(x),",d") if x != "" else "")
+    Results1= Results1.drop(["LnGrpDelay","ApproachDelay"],axis=1)
+    Results1 = Results1.sort_index()
+    return(Results1,Dat2)
+
+
+
+# Common path to the Synchro Files 
+fi = os.path.abspath('C:\\Users\\abibeka\\OneDrive - Kittelson & Associates, Inc\\Documents\\LPGA\\ToFDOT\\Synchro-Results\\Text Report')
+## Synchro file name
+fi_AM = os.path.join(fi,"LPGA-2025-AM.txt")
+
+## Read the tab delimited file
+file_object  = open(fi_AM, 'r')
+IntersectionsLineNO = []
+IntersectionsName = {}
+RawLines = []
+#https://www.geeksforgeeks.org/enumerate-in-python/
+for num, line in enumerate(file_object,0):
+    if(bool(re.search("^[1-6]:",line))):
+        IntersectionsLineNO.append(num)
+        IntersectionsName[num]=line
+        
+IntDetail_AM = {}
+IntSum_AM = {}
+NamesDict = {1:'Tomoka',2:'I95_SB',3:'I95_NB',4:'Technology',5:'Williamson',6:'N_Clyde'}
+# Tomoka i = 0 
+lab =1
+for Indx in IntersectionsLineNO:
+    IntDetail_AM[IntersectionsName[Indx]], IntSum_AM[IntersectionsName[Indx]] = GetRelevantDat(fi_AM,Indx)
+
+for i in IntersectionsLineNO:
+    print(IntersectionsName[i],IntSum_AM[IntersectionsName[i]])
+    print('\n----------------------------------------------------\n')
+    
+    
+    
+
+
+# Common path to the Synchro Files 
+fi = os.path.abspath('C:\\Users\\abibeka\\OneDrive - Kittelson & Associates, Inc\\Documents\\LPGA\\ToFDOT\\Synchro-Results\\Text Report')
+## Synchro file name
+fi_PM = os.path.join(fi,"LPGA-2025-AM.txt")
+
+## Read the tab delimited file
+file_object  = open(fi_PM, 'r')
+IntersectionsLineNO_PM = []
+IntersectionsName_PM = {}
+RawLines = []
+#https://www.geeksforgeeks.org/enumerate-in-python/
+for num, line in enumerate(file_object,0):
+    if(bool(re.search("^[1-6]:",line))):
+        IntersectionsLineNO_PM.append(num)
+        IntersectionsName_PM[num]=line
+        
+IntDetail_PM = {}
+IntSum_PM = {}
+NamesDict = {1:'Tomoka',2:'I95_SB',3:'I95_NB',4:'Technology',5:'Williamson',6:'N_Clyde'}
+# Tomoka i = 0 
+lab =1
+for Indx in IntersectionsLineNO_PM:
+    IntDetail_PM[IntersectionsName_PM[Indx]], IntSum_PM[IntersectionsName_PM[Indx]] = GetRelevantDat(fi_PM,Indx)
+
+for i in IntersectionsLineNO_PM:
+    print(IntersectionsName_PM[i],IntSum_PM[IntersectionsName_PM[i]])
+    print('\n----------------------------------------------------\n')
+    
+    
--- LPGA-Synchro-Read.py
+++ LPGA-Synchro-Read.py
@@ -19,93 +19,174 @@ import numpy as np
 import pandas as pd
 import re
 from io import StringIO
+
+ 
+def ReadIntData(i,Intersections,Dat):
+    '''
+    i is intersection #. i =0 is Tomoka
+    '''
+    Dat_Tomoka = Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+    #Get total intersection delay and LOS
+    IntStat_Tomoka = Dat.iloc[Intersections[i]+43:Intersections[i]+46,:]
+    IntStat_Tomoka = IntStat_Tomoka.iloc[:,0].apply(lambda x: x.replace('\t',''))
+    
+    
+    # Replace comma's with "-" to write file into csv
+    Dat_Tomoka.iloc[:,0] = Dat_Tomoka.iloc[:,0].apply(lambda x: x.replace(',','-'))
+    # Split by tab and replace with comma's 
+    Da_str = Dat_Tomoka.iloc[:,0].apply(lambda x: ','.join(x.split('\t'))+'\n')
+    # Output to a buffer line by line 
+    output = StringIO()
+    for i in range(Da_str.shape[0]):
+        output.write(Da_str.iloc[i])
+    contents = output.getvalue()
+    # Read the clean buffer
+    Dat_Clean = pd.read_csv(StringIO(contents))
+    output.close()
+    # Remove space from columns
+    Dat_Clean.columns =[col.strip() for col in Dat_Clean.columns]
+    # Get only rows I need and transpose
+    Results = Dat_Clean[Dat_Clean.Movement.isin(['V/C Ratio(X)          ','LnGrp Delay(d)-s/veh  ',
+                                       'LnGrp LOS             ','' ,'%ile BackOfQ(95%)-veh/ln',
+                                       'Approach Delay- s/veh ','Approach LOS          '])].transpose()
+    #Correct the columns
+    Results.columns = Results.iloc[0]
+    Results =Results.drop(Results.index[0])
+    Results.columns = ["V/C_Ratio","Q_95Per","LnGrpDelay","LnGrpLOS","ApproachDelay","ApproachLOS"]
+    Results["QLen"] = Results.Q_95Per.apply(pd.to_numeric, errors='coerce').round(0)*25
+    
+    Results1 =  Results[["V/C_Ratio","LnGrpDelay","LnGrpLOS","QLen","ApproachDelay","ApproachLOS"]]
+    
+    
+    Results1.index =pd.Categorical(Results1.index,[ 'WBL','WBT', 'WBR',
+                                   'NBL', 'NBT','NBR', 
+                                   'EBL', 'EBT', 'EBR', 
+                                   'SBL', 'SBT', 'SBR'])
+        
+    Results1.LnGrpLOS =Results1[['LnGrpDelay','LnGrpLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
+    
+    Results1.ApproachLOS =Results1[['ApproachDelay','ApproachLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
+    Results1.QLen = Results1.QLen.apply(lambda x: int(x) if x == x else "")
+    Results1.QLen = Results1.QLen.apply(lambda x: format(int(x),",d") if x != "" else "")
+    Results1= Results1.drop(["LnGrpDelay","ApproachDelay"],axis=1)
+    Results1 = Results1.sort_index()
+    IntStat_Tomoka
+    return(Results1,IntStat_Tomoka)
+
+
+
 # Common path to the Synchro Files 
 fi = os.path.abspath('C:\\Users\\abibeka\\OneDrive - Kittelson & Associates, Inc\\Documents\\LPGA\\ToFDOT\\Synchro-Results\\Text Report')
 ## Synchro file name
-fi_IN = os.path.join(fi,"LPGA-2025-PM.txt")
+fi_AM = os.path.join(fi,"LPGA-2025-AM.txt")
 ## Read the tab delimited file
-Dat = pd.read_fwf(fi_IN,header=None)
+DatMessyAM = pd.read_fwf(fi_AM,header=None)
 ## Find the intersection
-Dat["FlagInt"] = Dat[0].apply(lambda x: bool(re.search("^[1-6]:",x)))
+DatMessyAM["FlagInt"] = DatMessyAM[0].apply(lambda x: bool(re.search("^[1-6]:",x)))
 ## Create the start point of intersection 
-Intersections = Dat.index[Dat.FlagInt]
+Intersections = DatMessyAM.index[DatMessyAM.FlagInt]
+IntersectionName = DatMessyAM[DatMessyAM.FlagInt]
 # Get started with 1st intersection (Tomoka)
+
+## Create the start point of intersection 
+Intersections = DatMessyAM.index[DatMessyAM.FlagInt]
+IntersectionName = DatMessyAM[DatMessyAM.FlagInt]
+
+IntDetail_AM = {}
+IntSum_AM = {}
+NamesDict = {1:'Tomoka',2:'I95_SB',3:'I95_NB',4:'Technology',5:'Williamson',6:'N_Clyde'}
+# Tomoka i = 0 
+lab =1
+for key,val in NamesDict.items():
+    IntDetail_AM[key], IntSum_AM[key] = ReadIntData(key-1,Intersections,Dat= DatMessyAM)
+    lab =lab+1
+
+
+# Tomoka i = 0
 i = 1
-Dat_Tomoka = Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
-#Get total intersection delay and LOS
-IntStat_Tomoka = Dat.iloc[Intersections[i]+43:Intersections[i]+46,:]
-IntStat_Tomoka = IntStat_Tomoka.iloc[:,0].apply(lambda x: x.replace('\t',''))
+IntDetail_AM[i]
+IntSum_AM[i]
 
-
-# Replace comma's with "-" to write file into csv
-Dat_Tomoka.iloc[:,0] = Dat_Tomoka.iloc[:,0].apply(lambda x: x.replace(',','-'))
-# Split by tab and replace with comma's 
-Da_str = Dat_Tomoka.iloc[:,0].apply(lambda x: ','.join(x.split('\t'))+'\n')
-# Output to a buffer line by line 
-output = StringIO()
-for i in range(Da_str.shape[0]):
-    output.write(Da_str.iloc[i])
-contents = output.getvalue()
-# Read the clean buffer
-Dat_Clean = pd.read_csv(StringIO(contents))
-output.close()
-# Remove space from columns
-Dat_Clean.columns =[col.strip() for col in Dat_Clean.columns]
-# Get only rows I need and transpose
-Results = Dat_Clean[Dat_Clean.Movement.isin(['V/C Ratio(X)          ','LnGrp Delay(d)-s/veh  ',
-                                   'LnGrp LOS             ','' ,'%ile BackOfQ(95%)-veh/ln',
-                                   'Approach Delay- s/veh ','Approach LOS          '])].transpose()
-#Correct the columns
-Results.columns = Results.iloc[0]
-Results =Results.drop(Results.index[0])
-Results.columns = ["V/C_Ratio","Q_95Per","LnGrpDelay","LnGrpLOS","ApproachDelay","ApproachLOS"]
-Results["QLen"] = Results.Q_95Per.apply(pd.to_numeric, errors='coerce').round(0)*25
-
-Results1 =  Results[["V/C_Ratio","LnGrpDelay","LnGrpLOS","QLen","ApproachDelay","ApproachLOS"]]
-
-
-Results1.index =pd.Categorical(Results1.index,[ 'WBL','WBT', 'WBR',
-                               'NBL', 'NBT','NBR', 
-                               'EBL', 'EBT', 'EBR', 
-                               'SBL', 'SBT', 'SBR'])
-    
-Results1.LnGrpLOS =Results1[['LnGrpDelay','LnGrpLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
-
-Results1.ApproachLOS =Results1[['ApproachDelay','ApproachLOS']].apply(lambda x: '{} [{}]'.format(x[0],x[1]),axis=1)
-
-Results1 = Results1.sort_index()
-IntStat_Tomoka
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-i = 1
-Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+# SB Ramp 
 i = 2
-Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+IntDetail_AM[i]
+IntSum_AM[i]
+
+# NB Ramp 
 i = 3
-Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+IntDetail_AM[i]
+IntSum_AM[i]
+
+# Technology 
 i = 4
-Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+IntDetail_AM[i]
+IntSum_AM[i]
 
+# Williamson
 i = 5
-Dat.iloc[Intersections[i]+1:Intersections[i]+36,:]
+IntDetail_AM[i]
+IntSum_AM[i]
 
+# N_Clyde 
+i = 6
+IntDetail_AM[i]
+IntSum_AM[i]
+
+# Common path to the Synchro Files 
+fi = os.path.abspath('C:\\Users\\abibeka\\OneDrive - Kittelson & Associates, Inc\\Documents\\LPGA\\ToFDOT\\Synchro-Results\\Text Report')
+## Synchro file name
+fi_PM = os.path.join(fi,"LPGA-2025-PM.txt")
+## Read the tab delimited file
+DatMessyPM = pd.read_fwf(fi_PM,header=None)
+## Find the intersection
+DatMessyPM["FlagInt"] = DatMessyPM[0].apply(lambda x: bool(re.search("^[1-6]:",x)))
+## Create the start point of intersection 
+Intersections = DatMessyPM.index[DatMessyPM.FlagInt]
+IntersectionName = DatMessyPM[DatMessyPM.FlagInt]
+# Get started with 1st intersection (Tomoka)
+
+## Create the start point of intersection 
+Intersections = DatMessyPM.index[DatMessyPM.FlagInt]
+IntersectionName = DatMessyPM[DatMessyPM.FlagInt]
+IntDetail_PM = {}
+IntSum_PM = {}
+NamesDict = {1:'Tomoka',2:'I95_SB',3:'I95_NB',4:'Technology',5:'Williamson',6:'N_Clyde'}
+# Tomoka i = 0 
+lab =1
+for key,val in NamesDict.items():
+    IntDetail_PM[key], IntSum_PM[key] = ReadIntData(key-1,Intersections,Dat= DatMessyPM)
+    lab =lab+1
+
+
+# Tomoka i = 1
+i = 1
+IntDetail_PM[i]
+IntSum_PM[i]
+
+# SB Ramp 
+i =2
+IntDetail_PM[i]
+IntSum_PM[i]
+
+# NB Ramp 
+i = 3
+IntDetail_PM[i]
+IntSum_PM[i]
+
+# Technology 
+i = 4
+IntDetail_PM[i]
+IntSum_PM[i]
+
+# Williamson
+i = 5
+IntDetail_PM[i]
+IntSum_PM[i]
+
+# N_Clyde 
+i = 6
+IntDetail_PM[i]
+IntSum_PM[i]
 
 
 
